b24cc1aa9016da0d61f0cb9bd8aa7c28697cf966
mirko1593 <mirko1593@hotmail.com>
2024-08-17 02:41:40 +0800

    fix fonts requirements and npm requirement

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -16,7 +16,7 @@ return {
 					"branch",
 					"diff",
 					function()
-						return "󰅭 " .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.buf_get_clients())) or "")
+						return "󰅭 " .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.get_clients())) or "")
 					end,
 					{ "diagnostics", sources = { "nvim_diagnostic" } },
 					-- "diagnostics",
